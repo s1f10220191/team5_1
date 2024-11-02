5b4b3fae503514fc2d56db01292b428ae302043e
1F10220111 <s1F102201111@iniad.org>
2024-11-02 09:34:13 +0900

    BGM

--- team5_1/algostudy/static/algostudy/js/script_stage_template.js
+++ team5_1/algostudy/static/algostudy/js/script_stage_template.js
@@ -160,3 +160,28 @@ function drop(ev) {
     const direction = data.split('-')[1];
     commands.push(direction);
 }
+
+const bgm = document.getElementById('BGM')
+bgm.volume = 0.1;
+
+function audio_play(){
+    document.getElementById('btn-audio').currentTime = 0;
+    document.getElementById('btn-audio').play();
+}
+
+document.addEventListener('click', function(event){
+    if (event.button === 0){
+        audio_play();
+    }
+});
+
+
+function toggleBGM() {
+    if (bgm.paused) {
+        bgm.play();
+      document.getElementById('toggleBGM').textContent = 'BGMを停止'; // ボタンのテキストを変更
+    } else {
+        bgm.pause();
+      document.getElementById('toggleBGM').textContent = 'BGMを再生'; // ボタンのテキストを変更
+    }
+}
